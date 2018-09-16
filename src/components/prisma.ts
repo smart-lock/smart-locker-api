f1013bb8eb2d5711cc10c8f56c35081a78874962
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-16 17:55:06 -0300

    Prisma component

---
+++ src/components/prisma.ts
@@ -0,0 +1,30 @@
+import { IConfigComponent } from './config'
+import { ILifecycle } from '~/components/lifecycle';
+
+export interface IPrismaComponent<T> {
+  db: T
+}
+
+export interface IPrismaComponentDependencies {
+  config: IConfigComponent<{ prisma: { endpoint: string, debug: boolean } }>
+}
+
+export class PrismaComponent<T> implements ILifecycle, IPrismaComponent<T> {
+  public db: T
+  private PrismaClass: new (args: any) => T
+
+  constructor(PrismaClass: any) {
+    this.PrismaClass = PrismaClass
+  }
+
+  public async start(dependencies: IPrismaComponentDependencies) {
+    const { endpoint, debug } = dependencies.config.getConfig().prisma
+    this.db = new this.PrismaClass({
+      endpoint,
+      debug,
+    })
+  }
+  public async stop() {
+    // noop
+  }
+}
