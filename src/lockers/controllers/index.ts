c1a21231541c6ea4775a5631b8488ff10535b9a9
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-20 18:28:29 -0300

    Exporting locker controllers

--- src/lockers/controllers/index.ts
+++ src/lockers/controllers/index.ts
@@ -1,5 +1,11 @@
 import { claimLocker } from "~/lockers/controllers/claim-locker";
+import { unclaimLocker } from "~/lockers/controllers/unclaim-locker";
+import { lockLocker } from "~/lockers/controllers/lock-locker";
+import { unlockLocker } from "~/lockers/controllers/unlock-locker";
 
 export const lockerControllers = {
   claimLocker,
+  unclaimLocker,
+  lockLocker,
+  unlockLocker,
 }
