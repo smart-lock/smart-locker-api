2a118f480139434b49b2ea44971e79cf8a404f57
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-20 18:27:59 -0300

    Added mutations to resolvers

--- src/lockers/resolvers/mutation.ts
+++ src/lockers/resolvers/mutation.ts
@@ -5,8 +5,28 @@ export interface IClaimLockerArgs {
   lockerId: string
 }
 
+export interface IUnclaimLockerArgs {
+  lockerId: string
+}
+
+export interface ILockLockerArgs {
+  lockerId: string
+}
+export interface IUnlockLockerArgs {
+  lockerId: string
+}
+
 export const lockerMutation = {
-  claimLocker: (value, args: IClaimLockerArgs, ctx: IContext, info) => {
+  claimLocker: (_, args: IClaimLockerArgs, ctx: IContext) => {
     return lockerControllers.claimLocker(args.lockerId, ctx.account, ctx.components)
-  }
+  },
+  unclaimLocker: (_, args: IUnclaimLockerArgs, ctx: IContext) => {
+    return lockerControllers.unclaimLocker(args.lockerId, ctx.account, ctx.components)
+  },
+  lockLocker: (_, args: ILockLockerArgs, ctx: IContext) => {
+    return lockerControllers.lockLocker(args.lockerId, ctx.account, ctx.components)
+  },
+  unlockLocker: (_, args: IUnlockLockerArgs, ctx: IContext) => {
+    return lockerControllers.unlockLocker(args.lockerId, ctx.account, ctx.components)
+  },
 }
