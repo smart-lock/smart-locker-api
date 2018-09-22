ea2f43ec83b7fcd7c8a87fa30952e34975fcea38
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-22 17:01:19 -0300

    Added subscription resolver

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -15,4 +15,31 @@ export const resolvers = {
   Mutation: {
     ...lockerMutation,
   },
+  Subscription: {
+    lockerState: {
+      subscribe: async (_, { lockerId }, { components }: IContext) => {
+        const pubsubInstance = components.pubsub.instance
+        return pubsubInstance.asyncIterator(`lockers.${lockerId}`)
+      },
+    }
+  },
 };
+
+/*
+Should we send a first message about the locker state upon subscription ?
+
+setTimeout(async () => {
+  const locker = await components.prismaClient.db.locker({
+    id: lockerId,
+  })
+
+  pubsubInstance.publish(`lockers.${lockerId}`, {
+    lockerState: {
+      busy: locker.busy,
+      locked: locker.locked,
+      closed: locker.closed,
+      alarm: locker.alarm,
+    }
+  })
+})
+*/
