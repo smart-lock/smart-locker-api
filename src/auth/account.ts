75012911bd3140c611a65a0529c41e3058d6a64d
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-22 17:00:34 -0300

    extract authorization header from Connection also

--- src/auth/account.ts
+++ src/auth/account.ts
@@ -1,5 +1,7 @@
 import { ContextParameters } from "graphql-yoga/dist/types";
 import { IComponents } from "~/system";
+import { Nullable } from "~/common/types";
+import { IncomingHttpHeaders } from "http2";
 
 export interface IAccount {
   id: string
@@ -7,8 +9,26 @@ export interface IAccount {
   scopes: string[]
 }
 
-export const accountFromReq = async ({ request }: ContextParameters, deps: IComponents): Promise<IAccount | null> => {
-  const authorization = request.headers && request.headers.authorization
+const getHeadersFromCtxParameters = (ctxParameters: ContextParameters): Nullable<IncomingHttpHeaders> => {
+  const { request, connection } = ctxParameters
+  if (request) {
+    return request.headers
+  }
+
+  if (connection) {
+    return connection.context
+  }
+
+  return null
+}
+
+export interface IExpectedHeaders {
+  authorization: string
+}
+
+export const accountFromReq = async (ctxParameters: ContextParameters, deps: IComponents): Promise<IAccount | null> => {
+  const headers = getHeadersFromCtxParameters(ctxParameters)
+  const authorization = headers && headers.authorization
   if (!authorization || Array.isArray(authorization)) {
     return null
   }
