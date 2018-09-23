305977f843a5af54ee0a3f6599530d3ce527ae86
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-22 22:32:40 -0300

    myLockers & mySessions query

--- src/lockers/resolvers/query.ts
+++ src/lockers/resolvers/query.ts
@@ -1,4 +1,24 @@
 import { IContext } from "~/graphql/context";
+import { GraphQLResolveInfo } from "graphql";
 
 export const lockerQuery = {
+  myLockers: async (value, args, { components, account }: IContext, info: GraphQLResolveInfo) => {
+    return components.prismaBinding.db.query.lockers({
+      where: {
+        currentOwner: {
+          id: account.id,
+        }
+      }
+    }, info)
+  },
+  mySessions: async (value, args, { components, account }: IContext, info: GraphQLResolveInfo) => {
+    return components.prismaBinding.db.query.lockerSessions({
+      where: {
+        user: {
+          id: account.id
+        },
+        state: 0,
+      }
+    }, info)
+  },
 }
