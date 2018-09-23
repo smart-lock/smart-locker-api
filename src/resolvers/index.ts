d0a04ff27172b78a1440e556a82461eff7561191
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-22 22:32:07 -0300

    myLockers subscription resolver

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,45 +1,43 @@
 import { IContext } from "~/graphql/context";
 import { lockerQuery } from "~/lockers/resolvers/query";
 import { lockerMutation } from "~/lockers/resolvers/mutation";
-import { lockerSession } from "~/lockers/resolvers/locker-session";
 
 export const resolvers = {
   Query: {
-    hello: async (value, args, ctx: IContext) => {
-      // await ctx.mqtt.publish('inTopic', '1')
-      // return ctx.account.email
-    },
     ...lockerQuery
   },
-  ...lockerSession,
   Mutation: {
     ...lockerMutation,
   },
   Subscription: {
+    myLockers: {
+      subscribe: (_, __, { components, account }: IContext) => {
+        const pubsubInstance = components.pubsub.instance
+        const channel = `${account.id}.lockers`
+        return pubsubInstance.asyncIterator(channel)
+      }
+    },
     lockerState: {
       subscribe: async (_, { lockerId }, { components }: IContext) => {
         const pubsubInstance = components.pubsub.instance
-        return pubsubInstance.asyncIterator(`lockers.${lockerId}`)
+        const channel = `lockers.${lockerId}`
+        console.log(channel)
+        setTimeout(async () => {
+          const locker = await components.prismaClient.db.locker({
+            id: lockerId,
+          })
+        
+          pubsubInstance.publish(`lockers.${lockerId}`, {
+            lockerState: {
+              busy: locker.busy,
+              locked: locker.locked,
+              closed: locker.closed,
+              alarm: locker.alarm,
+            }
+          })
+        })
+        return pubsubInstance.asyncIterator(channel)
       },
     }
   },
 };
-
-/*
-Should we send a first message about the locker state upon subscription ?
-
-setTimeout(async () => {
-  const locker = await components.prismaClient.db.locker({
-    id: lockerId,
-  })
-
-  pubsubInstance.publish(`lockers.${lockerId}`, {
-    lockerState: {
-      busy: locker.busy,
-      locked: locker.locked,
-      closed: locker.closed,
-      alarm: locker.alarm,
-    }
-  })
-})
-*/
