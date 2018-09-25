bb17fa578bc99c309f28503ef8b5efe87f871751
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-25 15:36:31 -0300

    cleanup

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,10 +1,6 @@
 import { ContextParameters } from 'graphql-yoga/dist/types';
-import { prisma, Prisma } from '~/prisma-client'
-import { Prisma as PrismaBinding } from '~/generated/prisma'
-import { IMQTTComponent } from '~/components/mqtt';
 import { IComponents } from '~/system';
 import { IAccount, accountFromReq } from '~/auth/account';
-import { PrismaComponent } from '~/components/prisma';
 
 export interface IContext {
   account: IAccount
