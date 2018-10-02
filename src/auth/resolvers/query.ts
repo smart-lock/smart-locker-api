77febd2de674ee0405d1fb1860e08069fdb50afa
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-10-02 18:24:44 -0300

    Fix types

--- src/auth/resolvers/query.ts
+++ src/auth/resolvers/query.ts
@@ -1,9 +1,9 @@
 import { GraphQLResolveInfo } from "graphql";
-import { IContext } from "~/graphql/context";
+import { IAuthenticatedContext } from "~/graphql/context";
 import * as Boom from 'boom'
 
 export const authQuery = {
-  me: async (_, __, { components, account }: IContext, info: GraphQLResolveInfo) => {
+  me: async (_, __, { components, account }: IAuthenticatedContext, info: GraphQLResolveInfo) => {
     const user = await components.prismaBinding.db.query.user({ where: { id: account.id }}, info)
     if (!user) {
       throw Boom.notFound('UserNotFound')
--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -3,10 +3,15 @@ import { IComponents } from '~/system';
 import { IAccount, accountFromReq } from '~/auth/account';
 
 export interface IContext {
+  components: IComponents
+}
+
+export interface IAuthenticatedContext {
   account: IAccount
   components: IComponents
 }
-export const contextFromReq = async (req: ContextParameters, deps: IComponents): Promise<IContext> => {
+
+export const contextFromReq = async (req: ContextParameters, deps: IComponents): Promise<IAuthenticatedContext | IContext> => {
   return {
     components: deps,
     account: await accountFromReq(req, deps)
