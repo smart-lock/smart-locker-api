e6cb3aac11a2d708ef1b6115197ea164c2fa4872
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-10-02 18:52:25 -0300

    Using auth query & mutation on schema resolvers

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,13 +1,17 @@
 import { lockerQuery } from "~/lockers/resolvers/query";
 import { lockerMutation } from "~/lockers/resolvers/mutation";
 import { lockerSubscriptions } from "~/auth/resolvers/subscriptions";
+import { authQuery } from "~/auth/resolvers/query";
+import { authMutation } from "~/auth/resolvers/mutation";
 
 export const resolvers = {
   Query: {
-    ...lockerQuery
+    ...lockerQuery,
+    ...authQuery,
   },
   Mutation: {
     ...lockerMutation,
+    ...authMutation,
   },
   Subscription: {
     ...lockerSubscriptions,
