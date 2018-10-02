424c77c68a943989429ab6c7112ee9c294c4f5a3
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-10-02 18:39:59 -0300

    Using authenticated middleware on me

--- src/auth/resolvers/query.ts
+++ src/auth/resolvers/query.ts
@@ -1,14 +1,19 @@
 import { GraphQLResolveInfo } from "graphql";
 import { IAuthenticatedContext } from "~/graphql/context";
 import * as Boom from 'boom'
+import { combineResolvers } from 'graphql-resolvers'
+import { authenticated } from "~/auth/middlewares";
 
 export const authQuery = {
-  me: async (_, __, { components, account }: IAuthenticatedContext, info: GraphQLResolveInfo) => {
-    const user = await components.prismaBinding.db.query.user({ where: { id: account.id }}, info)
-    if (!user) {
-      throw Boom.notFound('UserNotFound')
+  me: combineResolvers(
+    authenticated,
+    async (_, __, { components, account }: IAuthenticatedContext, info: GraphQLResolveInfo) => {
+      const user = await components.prismaBinding.db.query.user({ where: { id: account.id }}, info)
+      if (!user) {
+        throw Boom.notFound('UserNotFound')
+      }
+  
+      return user
     }
-
-    return user
-  }
+  )
 }
