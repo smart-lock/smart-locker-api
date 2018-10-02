c5beefba77af374d0d3dbe2eb3185da9e1be3363
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-10-02 18:52:00 -0300

    auth d

---
+++ src/auth/db.ts
@@ -0,0 +1,19 @@
+import { PrismaClientComponent } from '~/components/prisma-client';
+import { Prisma } from '~/prisma-client';
+import * as bcrypt from 'bcryptjs'
+
+export const findUserByEmail = (email: string, prismaClient: PrismaClientComponent<Prisma>) => {
+  return prismaClient.db.user({
+    email,
+  })
+}
+
+export const createUser = async (email: string, password: string, name: string, prismaClient: PrismaClientComponent<Prisma>) => {
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  return prismaClient.db.createUser({
+    email,
+    password: hashedPassword,
+    name,
+  })
+}
