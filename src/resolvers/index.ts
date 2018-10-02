7caf20e8b64cab3090b005cecc08ccad74009b1e
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-10-02 18:40:29 -0300

    Extracted subscriptions

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,6 +1,6 @@
-import { IContext } from "~/graphql/context";
 import { lockerQuery } from "~/lockers/resolvers/query";
 import { lockerMutation } from "~/lockers/resolvers/mutation";
+import { lockerSubscriptions } from "~/auth/resolvers/subscriptions";
 
 export const resolvers = {
   Query: {
@@ -10,34 +10,6 @@ export const resolvers = {
     ...lockerMutation,
   },
   Subscription: {
-    myLockers: {
-      subscribe: (_, __, { components, account }: IContext) => {
-        const pubsubInstance = components.pubsub.instance
-        const channel = `${account.id}.lockers`
-        return pubsubInstance.asyncIterator(channel)
-      }
-    },
-    lockerState: {
-      subscribe: async (_, { lockerId }, { components }: IContext) => {
-        const pubsubInstance = components.pubsub.instance
-        const channel = `lockers.${lockerId}`
-        console.log(channel)
-        setTimeout(async () => {
-          const locker = await components.prismaClient.db.locker({
-            id: lockerId,
-          })
-        
-          pubsubInstance.publish(`lockers.${lockerId}`, {
-            lockerState: {
-              busy: locker.busy,
-              locked: locker.locked,
-              closed: locker.closed,
-              alarm: locker.alarm,
-            }
-          })
-        })
-        return pubsubInstance.asyncIterator(channel)
-      },
-    }
+    ...lockerSubscriptions,
   },
 };
